9926e18c5c0f78163aa514bb825baa89b34b303a
dzyradev <116470817+DzyraDev@users.noreply.github.com>
2026-03-16 15:15:32 +0700

    implement homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
 import Navbar from "../../components/Navbar";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "../../components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,10 +11,91 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-      <div className="home">
-        <Navbar />
-        <h2 className="text-3xl text-indigo-700 font-extrabold">Home</h2>
+    <div className="home">
+      <Navbar />
 
-      </div>
-  )
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Roomiqys 2.0</p>
+        </div>
+
+        <h1>Build beautiful spaces at the speed of thought with Roomiqys</h1>
+
+        <p className="subtitle">
+          Roomiqys is an AI-First design environment that helps you visualize,
+          render, and ship architectural projects faster than ever
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Support JPG, PNG, formats up to 10MB</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+      {/*<h2 className="text-3xl text-indigo-700 font-extrabold">Home</h2>*/}
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Project</h2>
+              <p>
+                Your latest work and shared community project, all in one place.
+              </p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="project"
+                />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("01.01.2027").toDateString()}</span>
+                    <span>By DzyraTechnology</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
